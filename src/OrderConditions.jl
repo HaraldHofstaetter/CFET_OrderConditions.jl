3740cd914f64e78e071c52a0be0efa2daadcec26
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2016-11-06 01:33:38 +0100

    expand commutators

--- src/OrderConditions.jl
+++ src/OrderConditions.jl
@@ -7,10 +7,10 @@ export gen_exp_tBt_derivatives_coeffs
 export gen_C_derivatives_at_0
 export gen_C_derivatives_at_0_in_terms_of_A
 export gen_exp_tBt_derivatives_at_0_in_terms_of_A
+export expand_commutators
 export gen_CFET_order_conditions
 
 
-# package code goes here
 
 function gen_exp_tBt_derivatives_coeffs(N::Integer)
     A = [Dict{Array{Int64,1},Int64}([0]=>1)]
@@ -71,20 +71,21 @@ function gen_C_derivatives_at_0(N::Integer)
 end
 
 
-function gen_C_derivatives_at_0_in_terms_of_A(N::Integer, b::Array{giac,1})
+function gen_C_derivatives_at_0_in_terms_of_A{T}(N::Integer, b::Array{T,1})
     C_derivatives_at_0=gen_C_derivatives_at_0(N)
-    C1 = [Dict{Array{Int64,1},giac}() for n=0:N]
+    C1 = [Dict{Array{Int64,1},T}() for n=0:N]
     for n = 0:N
         for (p,coeff) in C_derivatives_at_0[n+1]
-            C1[n+1][p] = factor(giac(coeff) * prod([b[p[j]+1]  for j =1:length(p)]))
+            C1[n+1][p] = coeff * prod([b[p[j]+1]  for j =1:length(p)])
         end
     end
     C1
 end
 
 
-function mult(x::Dict{Array{Array{Int64,1},1},Giac.giac}, y::Dict{Array{Int64,1},Giac.giac})
-    r = Dict{Array{Array{Int64,1},1},Giac.giac}()
+
+function mult{T}(x::Dict{Array{Array{Int64,1},1},T}, y::Dict{Array{Int64,1},T})
+    r = Dict{Array{Array{Int64,1},1},T}()
     for (p,c) in x
         for (q, d) in y
             pq = copy(p)
@@ -96,8 +97,8 @@ function mult(x::Dict{Array{Array{Int64,1},1},Giac.giac}, y::Dict{Array{Int64,1}
 end
 
 
-function mult(x::Dict{Array{Array{Int64,1},1},Giac.giac}, y::Dict{Array{Array{Int64,1},1},Giac.giac})
-    r = Dict{Array{Array{Int64,1},1},Giac.giac}()
+function mult{T}(x::Dict{Array{Array{Int64,1},1},T}, y::Dict{Array{Array{Int64,1},1},T})
+    r = Dict{Array{Array{Int64,1},1},T}()
     for (p,c) in x
         for (q, d) in y
             pq = vcat(p,q)
@@ -108,23 +109,24 @@ function mult(x::Dict{Array{Array{Int64,1},1},Giac.giac}, y::Dict{Array{Array{In
 end
 
 
-function add(x::Dict{Array{Array{Int64,1},1},Giac.giac}, y::Dict{Array{Array{Int64,1},1},Giac.giac})
+function add{T}(x::Dict{Array{Array{Int64,1},1},T}, y::Dict{Array{Array{Int64,1},1},T}; 
+                fx=one(T), fy=one(T))
     r = copy(x)
     for (p,c) in y
         d = get(r, p, 0)
-        r[p] = c+d
+        r[p] = fy*c + fx*d
     end
     r
 end
 
-function gen_exp_tBt_derivatives_at_0_in_terms_of_A(N::Integer, b::Array{giac,1}) 
+function gen_exp_tBt_derivatives_at_0_in_terms_of_A{T}(N::Integer, b::Array{T,1}) 
     C_derivatives_at_0_in_terms_of_A=gen_C_derivatives_at_0_in_terms_of_A(N-1, b)
     exp_tBt_derivatives_coeffs=gen_exp_tBt_derivatives_coeffs(N)
-    r = Dict{Array{Array{Int64,1},1},Giac.giac}[]
+    r = Dict{Array{Array{Int64,1},1},T}[]
     for n = 1:N
-        y = Dict{Array{Array{Int64,1},1},Giac.giac}()
+        y = Dict{Array{Array{Int64,1},1},T}()
         for (p,d) in exp_tBt_derivatives_coeffs[n]
-            x = Dict{Array{Array{Int64,1},1},Giac.giac}(
+            x = Dict{Array{Array{Int64,1},1},T}(
             [Array[p1]=>d*d1 for (p1,d1) in C_derivatives_at_0_in_terms_of_A[p[1]+1]])
             for i = 2:length(p)
                 x = mult(x, C_derivatives_at_0_in_terms_of_A[p[i]+1])
@@ -135,6 +137,33 @@ function gen_exp_tBt_derivatives_at_0_in_terms_of_A(N::Integer, b::Array{giac,1}
     end
     r
 end
+
+
+function expand_commutators{T}(a::Array{Int64,1}, f::T; coeff_type=T)
+    if length(a)>=3
+        b = Dict{Array{Array{Int64,1},1},coeff_type}(Array[[a[1]]]=>one(T))
+        c = expand_commutators(a[2:end], f, coeff_type=coeff_type)
+        return add(mult(b,c), mult(c,b), fy=-one(T))
+    elseif length(a)==2
+        return Dict{Array{Array{Int64,1},1},coeff_type}(Array[[a[1]], [a[2]]]=>f, Array[[a[2]], [a[1]]]=>-f)
+    else
+        return Dict{Array{Array{Int64,1},1},coeff_type}(Array[a]=>f)
+    end
+end
+
+
+function expand_commutators{T}(a::Dict{Array{Array{Int64,1},1},T})    
+    b = Dict{Array{Array{Int64,1},1},T}()
+    for (p,c) in a        
+        q = expand_commutators(p[1], one(T), coeff_type=T)
+        for i = 2:length(p)
+            q = mult(q, expand_commutators(p[i], one(T), coeff_type=T))
+        end
+        b = add(b, q, fy=c)
+    end
+    b
+end
+
 
 
 multinomial_coeff(q::Int, k::Array{Int,1}) = div(factorial(q), prod([factorial(i) for i in k]))
