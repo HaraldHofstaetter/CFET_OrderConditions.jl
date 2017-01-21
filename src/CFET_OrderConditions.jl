7f4ad2bcdfdf421fb9d9cd30574438f6599d34c2
HaraldHofstaetter <hofi@harald-hofstaetter.at>
2017-01-21 20:26:00 +0100

    gen_hall_basis_elements_of_grade

--- src/CFET_OrderConditions.jl
+++ src/CFET_OrderConditions.jl
@@ -14,7 +14,7 @@ export gen_CFET_order_conditions
 export number_commutators_of_grade_equal_to
 export number_commutators_of_grade_equal_or_less_than
 
-export integer_vector_weighted, lyndon_words
+export integer_vector_weighted, lyndon_words, gen_hall_basis_elements_of_grade
 
 typealias Commutator Array{Int64,1}
 typealias LCCC{T} Dict{Array{Array{Int64,1},1},T} # Linear Combination of Commutator Chains
@@ -194,7 +194,8 @@ function gen_CFET_order_conditions(N::Integer,J::Integer)
     for j=2:J
         push!(CC, [LCCC{giac}([key=>subst(val,b[1],b[j]) for (key,val) in c]) for c in C])
     end    
-    Y = LCCC{giac}[]
+    #Y = LCCC{giac}[]
+    Y = Dict{Array{Int64,1},giac}[]
     for q=0:N-1
         y = LCCC{giac}()
         for k0 in Combinatorics.WithReplacementCombinations(1:J,q+1)
@@ -222,7 +223,8 @@ function gen_CFET_order_conditions(N::Integer,J::Integer)
             y = add(y, x, fy=-one(giac))
             
         end   
-        push!(Y, LCCC{giac}([key=>factor(val) for (key,val) in y]))
+        #push!(Y, LCCC{giac}([key=>factor(val) for (key,val) in y]))
+        push!(Y, Dict{Array{Int64,1},giac}([[k[1] for k in key]=>factor(val) for (key,val) in y]))
     end
     Y
 end
@@ -390,7 +392,18 @@ function lyndon_words(e::Vector{Int})
     Vector{Int}[[i+k for i in z] for z in simple_fixed_content(e[k:end])]
 end
 
+#The following is stolen from http://mathoverflow.net/questions/97703/list-of-hall-basis
 
+function gen_hall_basis_elements_of_grade(g::Int)
+    L = integer_vector_weighted(g, collect(1:g))
+    res = Vector{Int}[]
+    for l in L
+        for w in lyndon_words(l)
+            push!(res, w-1)
+        end
+    end
+    res
+end
 
 
 end # module
